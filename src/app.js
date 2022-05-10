aaabcca4ec95c7fca45c21c02abfabedf235479e
Al <alpalma95@gmail.com>
2022-05-10 19:04:37 +0000

    created undo button

--- src/app.js
+++ src/app.js
@@ -1,11 +1,66 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const domainOptions = document.getElementById("domains");
+const btn = document.getElementById("generate");
+const list = document.getElementById("list");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const art = ["the", "a"];
+const adj = ["great", "worst", "fabulous", "nice", "crowded"];
+const noun = ["cinema", "film", "popcorn", "oscar"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const domainList = [
+  ".com",
+  ".es",
+  ".org",
+  ".net",
+  ".int",
+  ".mil",
+  ".edu",
+  ".ar",
+  ".pt",
+];
+
+//Render domain options
+const optionsForDomain = domainList.map((x) =>
+  document.createElement("option")
+);
+
+for (let i in domainList) {
+  optionsForDomain[i].value = domainList[i];
+  optionsForDomain[i].innerHTML = domainList[i];
+
+  domainOptions.appendChild(optionsForDomain[i]);
+}
+
+//Creating combinations
+const combinations = [];
+
+for (let i in art) {
+  for (let j in adj) {
+    for (let k in noun) {
+      combinations.push(`${art[i]}${adj[j]}${noun[k]}`);
+    }
+  }
+}
+
+//Rendering items
+let htmlElements, selectedDomain;
+
+const updateContent = () => {
+  list.innerHTML = "";
+  for (let i in htmlElements) {
+    list.innerHTML += htmlElements[i];
+  }
 };
+
+const generateList = () => {
+  selectedDomain = domainOptions.value;
+  // console.log(domainOptions.innerHTML);
+  htmlElements = combinations.map((x) => `<li>${x}${selectedDomain}</li>`);
+  updateContent();
+  list.classList.remove("hidden");
+};
+
+const removeAll = () => {
+  list.innerHTML = "";
+  list.classList.add("hidden");
+};
+//didn't have time for the other bonus of replacing the domain :(
